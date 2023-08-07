0eecac073b511e8ca46cebedb2f21548ce7ed596
kostis85 <129678217+kostis85@users.noreply.github.com>
2023-08-07 08:46:18 +0100

    adding variables

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
 package main
 
-Print("Hello World")
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T,remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are  still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+	var userName string
+	var userTickets int
+	//ask user for their name
+	fmt.Scan(userName)
+
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+}
